47d53807796c18a813a880682ff14db92436c00c
cristian.restituyo@friday.de <cristian.restituyo@friday.de>
2021-08-15 20:10:12 +0200

    feat: add save the date desktop

--- src/sections/Home/SaveTheDate/index.jsx
+++ src/sections/Home/SaveTheDate/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Countdown from "../../../components/Countdown";
 import Grid from "../../../components/Grid";
 import Text, { HeaderText } from "../../../components/Text";
 import "./SaveTheDate.scss";
@@ -8,7 +9,7 @@ const SaveTheDate = () => {
     <section className="saveTheDate">
       <Grid className="saveTheDate__row">
         <div className="saveTheDate__wrapper">
-          <div className="saveTheDate__column saveTheDate__leftColumn">
+          <div className="saveTheDate__column saveTheDate__columnLeft">
             <HeaderText className="saveTheDate__title">
               Save the Date
             </HeaderText>
@@ -18,6 +19,9 @@ const SaveTheDate = () => {
                 actualizaciones sobre nuestra boda.
               </Text>
             </div>
+          </div>
+          <div className="saveTheDate__column saveTheDate__countdownColumn">
+              <Countdown />
           </div>
         </div>
       </Grid>
