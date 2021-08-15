2bce3f4bda0a5d7f40cebbae9206562bfa34ea98
cristian.restituyo@friday.de <cristian.restituyo@friday.de>
2021-08-15 15:56:00 +0200

    feat: add grid

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,21 +1,48 @@
-import React from 'react'
-import Button from '../Button';
-import Text from '../Text';
-import './Nav.scss';
+import React from "react";
+import Button from "../Button";
+import Grid from "../Grid";
+import Text from "../Text";
+import "./Nav.scss";
 
 const Nav = () => {
-    return (
-        <nav className = "nav">
-            <ul className = "nav__list">
-                <li className = "nav__list-item"><a href=""><Text fontWeight="bold">Save the Date</Text></a></li>
-                <li className = "nav__list-item"><a href=""><Text fontWeight="bold">Regalos</Text></a></li>
-                <li className = "nav__list-item"><a href=""><Text fontWeight="bold">Q&A</Text></a></li>
-                <li className = "nav__list-item"><a href=""><Text fontWeight="bold">Galería</Text></a></li>
-                <li className = "nav__list-item"><a href=""><Text fontWeight="bold">Ubicación</Text></a></li>
-                <li className = "nav__list-item"><Button><Text>rsvp</Text></Button></li>
-            </ul>
-        </nav>
-    )
-}
+  return (
+    <nav className="nav">
+      <Grid>
+        <ul className="nav__list">
+          <li className="nav__list-item">
+            <a href="">
+              <Text fontWeight="bold">Save the Date</Text>
+            </a>
+          </li>
+          <li className="nav__list-item">
+            <a href="">
+              <Text fontWeight="bold">Regalos</Text>
+            </a>
+          </li>
+          <li className="nav__list-item">
+            <a href="">
+              <Text fontWeight="bold">Q&A</Text>
+            </a>
+          </li>
+          <li className="nav__list-item">
+            <a href="">
+              <Text fontWeight="bold">Galería</Text>
+            </a>
+          </li>
+          <li className="nav__list-item">
+            <a href="">
+              <Text fontWeight="bold">Ubicación</Text>
+            </a>
+          </li>
+          <li className="nav__list-item">
+            <Button>
+              <Text>rsvp</Text>
+            </Button>
+          </li>
+        </ul>
+      </Grid>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
