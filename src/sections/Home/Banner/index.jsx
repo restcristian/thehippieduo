dde2c57a040b7eb060676bb1380cf71efe369787
cristian.restituyo@friday.de <cristian.restituyo@friday.de>
2021-08-15 13:07:32 +0200

    feat: add image on the left

---
+++ src/sections/Home/Banner/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Banner.scss";
+
+const Banner = () => {
+    return (
+        <section className="banner">
+           <div className="banner__leftBG"></div>
+        </section>
+    )
+}
+
+export default Banner;
