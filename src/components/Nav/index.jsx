96811abbacc14003ce9735a2c7b320a2e41449b8
cristian.restituyo@friday.de <cristian.restituyo@friday.de>
2021-08-16 21:01:27 +0200

    fix: refactor navigation

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -4,36 +4,40 @@ import Grid from "../Grid";
 import Text from "../Text";
 import "./Nav.scss";
 
+const items = [
+  {
+    label: "Save the Date",
+    link: "#",
+  },
+  {
+    label: "Regalos",
+    link: "#",
+  },
+  {
+    label: "Q&A",
+    link: "#",
+  },
+  {
+    label: "Galería",
+    link: "#",
+  },
+  {
+    label: "Ubicación",
+    link: "#",
+  },
+];
 const Nav = () => {
   return (
     <nav className="nav">
       <Grid>
         <ul className="nav__list">
-          <li className="nav__list-item">
-            <a href="">
-              <Text fontWeight="bold">Save the Date</Text>
-            </a>
-          </li>
-          <li className="nav__list-item">
-            <a href="">
-              <Text fontWeight="bold">Regalos</Text>
-            </a>
-          </li>
-          <li className="nav__list-item">
-            <a href="">
-              <Text fontWeight="bold">Q&A</Text>
-            </a>
-          </li>
-          <li className="nav__list-item">
-            <a href="">
-              <Text fontWeight="bold">Galería</Text>
-            </a>
-          </li>
-          <li className="nav__list-item">
-            <a href="">
-              <Text fontWeight="bold">Ubicación</Text>
-            </a>
-          </li>
+          {items.map((item, index) => (
+            <li className="nav__list-item" key={`{nav__list-item-${index}}`}>
+              <a href={item.link}>
+                <Text fontWeight="bold">{item.label}</Text>
+              </a>
+            </li>
+          ))}
           <li className="nav__list-item">
             <Button>
               <Text>rsvp</Text>
