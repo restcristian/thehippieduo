52cb86b9e9ba15dfc44682621ea29e3ceff9af81
Cristian Restituyo <cristian.restituyo@gmail.com>
2021-09-15 01:20:22 +0200

    Fix add seconds to countdown (#10)
    * fix: add seconds
    
    * fix: add calendar button with responsivenss
    
    Co-authored-by: cristian.restituyo@friday.de <cristian.restituyo@friday.de>

--- src/sections/Home/SaveTheDate/index.jsx
+++ src/sections/Home/SaveTheDate/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "../../../components/Button";
 import Countdown from "../../../components/Countdown";
 import Grid from "../../../components/Grid";
 import Text, { HeaderText } from "../../../components/Text";
@@ -15,13 +16,20 @@ const SaveTheDate = () => {
             </HeaderText>
             <div className="saveTheDate__description">
               <Text fontWeight="bold">
-                Coloca tu propio recordatorio y mantente atento a todas las
-                actualizaciones sobre nuestra boda.
+                Coloca tu propio recordatorio y mantente atento
+                <br />a todas las actualizaciones sobre nuestra boda.
               </Text>
             </div>
           </div>
           <div className="saveTheDate__column saveTheDate__countdownColumn">
+            <div className = "saveTheDate__countdownColumnWrapper">
               <Countdown />
+              <div className="saveTheDate__addToCalendar">
+                <Button>
+                  <Text>Añade al calendario</Text>
+                </Button>
+              </div>
+            </div>
           </div>
         </div>
       </Grid>
