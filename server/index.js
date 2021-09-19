d0ed2e6eb9ad34b82d8901c546d6a7a06e7841e2
Cristian Restituyo <cristian.restituyo@gmail.com>
2021-09-19 19:28:28 +0200

    feat: add connection to database with insertion (#20)
    Co-authored-by: cristian.restituyo@friday.de <cristian.restituyo@friday.de>

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv');
+const {
+    Client
+} = require('pg')
 
 const app = express();
 const PORT = process.env.PORT || 9090;
+const connectionString = process.env.DATABASE_URL;
 
 const CLIENT_URL = 'http://localhost:8000';
-
 app.use(cors({
     origin: CLIENT_URL
 }))
@@ -14,16 +18,38 @@ app.get('/', (req, res) => {
     res.send('hello world')
 });
 
-app.post('/contact', (req, res) => {
+const client = new Client({
+    connectionString,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
+
+client.connect().catch(error => {
+    console.log(error)
+})
+
+app.post('/contact', async (req, res) => {
     const {
         name,
         email,
         message,
-        song
+        song = ''
     } = req.body;
-    console.log(req.body);
-    console.log('contact info received');
-    res.send('contact info received')
+
+    console.log(req.body)
+
+    try {
+        const query = {
+            text: 'INSERT INTO rsvp(email, name, message, song) VALUES($1,$2,$3,$4)',
+            values: [email, name, message, song]
+        }
+        const result = await client.query(query);
+        console.log(result);
+        client.end();
+    } catch (error) {
+        throw error;
+    }
 });
 
-app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`listening on port: ${PORT}`))
