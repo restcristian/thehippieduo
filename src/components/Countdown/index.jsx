c12c735a419b53f94642db78fd0b4772c960c3b0
Cristian Restituyo <cristian.restituyo@gmail.com>
2021-09-19 16:11:20 +0200

    refactor: make countdown functionality into hook (#17)
    Co-authored-by: cristian.restituyo@friday.de <cristian.restituyo@friday.de>

--- src/components/Countdown/index.jsx
+++ src/components/Countdown/index.jsx
@@ -1,51 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Text from "../Text";
+import { useCountDown, formatTime } from "../../shared/utils";
 import "./Countdown.scss";
 
 const Countdown = () => {
-  const [hours, setHours] = useState(0);
-  const [days, setDays] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
+  const { days, hours, minutes, seconds } = useCountDown();
 
-  const countdownDate = new Date("April 14, 2022 15:00:00");
-  let timeInterval;
-
-  const timePass = () => {
-    // Get today's time
-    const now = new Date().getTime();
-
-    // distance between now and countdownDate
-    const distance = countdownDate - now;
-
-    // Formateted time
-    const fDays = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const fHours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const fMinutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const fSeconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    setDays(fDays);
-    setHours(fHours);
-    setMinutes(fMinutes);
-    setSeconds(fSeconds);
-    // if the date has arrived
-    if (distance < 0) {
-      clearInterval(timeInterval);
-    }
-  };
-
-  const formatTime = (time) => {
-    if (parseInt(time) >= 0 && parseInt(time) <= 9) {
-      return `0${time}`;
-    }
-    return time;
-  };
-
-  useEffect(() => {
-    timeInterval = setInterval(timePass, 1000);
-  }, []);
   return (
     <div className="countdown">
       <div className="countdown__wrapper">
