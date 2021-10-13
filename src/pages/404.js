1b69c4a24cbd7f0a64b7a396d5fdb9497e9cd7f7
Cristian Restituyo <cristian.restituyo@gmail.com>
2021-10-13 20:03:52 +0200

    feat: add 404 page (#27)

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,34 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import Text, { HeaderText } from "../components/Text";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import "./404.scss";
+import "../shared/reset.scss";
+import Button from "../components/Button";
 
 // markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
+    <main className="notFound">
+      <div className="notFound__wrapper">
+        <HeaderText className="notFound__title" fontWeight="bold">
+          Eeeh...
+        </HeaderText>
+        <p className="notFound__description">
+          <Text className="notFound__descriptionText">
+            {" "}
+            Parece que no deberías estar aquí.
             <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
+            ¿Qué tal si regresas a la página principal?
+          </Text>
+        </p>
+        <div className="notFound__buttonWrapper">
+          <Button isLink redirectTo="/" isExternal={false}>
+            <Text>Sí, Llévame al inicio</Text>
+          </Button>
+        </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
