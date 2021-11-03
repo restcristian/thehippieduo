a0434a11dc2464a38c03e56f52fdd1fa78b25712
cristian.restituyo@friday.de <cristian.restituyo@friday.de>
2021-11-03 20:03:17 +0100

    fix: closes #33

--- src/sections/Home/GoodBye/index.jsx
+++ src/sections/Home/GoodBye/index.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import Grid from "../../../components/Grid";
 import Img from "../../../components/Img";
 import Text, { HeaderText } from "../../../components/Text";
-import andrysCristian from '../../../images/andrysandcristian@2x.png';
+import andrysCristianDesktop from "../../../images/andrysandcristian@2x.png";
+import andrysCristianMobile from "../../../images/andrys_cristian_mobile.png";
 import "./GoodBye.scss";
 
 const GoodBye = () => {
@@ -11,7 +12,14 @@ const GoodBye = () => {
       <Grid>
         <div className="goodbye__row">
           <HeaderText className="goodbye__title">Nos vemos pronto</HeaderText>
-          <Img src = {andrysCristian} className = "goodbye__image" alt = "Andrys & Cristian" />
+          <picture>
+            <source media="(min-width: 601px)" srcset={andrysCristianDesktop} />
+            <Img
+              src={andrysCristianMobile}
+              className="goodbye__image"
+              alt="Andrys & Cristian"
+            />
+          </picture>
           <p className="goodbye__description">
             <Text>
               Este es un gran día en nuestras vidas y tu presencia lo hace aún
